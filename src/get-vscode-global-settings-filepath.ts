d2046c61c3e05d6f3283fa466230b2786d980bc8
amittkSharma <amit12.sharma@gmail.com>
2024-08-30 23:29:04 +0200

    chore: code cleanup

--- src/get-vscode-global-settings-filepath.ts
+++ src/get-vscode-global-settings-filepath.ts
@@ -8,14 +8,3 @@ export const getVscodeGlobalSettingsFilePath = (platform: NodeJS.Platform, homed
       return `${homedir}/AppData\\Code\\User\\settings.json`;
   }
 };
-
-export const getVscodeGlobalExtensionsFilePath = (platform: NodeJS.Platform, homedir: string) => {
-  switch (platform) {
-    case 'darwin':
-      return `${homedir}/Library/Application Support/Code/User/extensions.json`;
-    case 'linux':
-      return `${homedir}/.config/Code/User/extensions.json`;
-    default:
-      return `${homedir}/AppData\\Code\\User\\extensions.json`;
-  }
-};
